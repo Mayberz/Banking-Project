3cdfa0a542b34bf3a2391269a0e47af8018d0fa4
sarfaraj <sarfa@192.168.1.42>
2024-06-19 11:43:44 +0530

    API Gateway

---
+++ TrustBank-05-ApiGateway/src/main/java/com/yo/Trust/TrustBank05ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.yo.Trust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustBank05ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustBank05ApiGatewayApplication.class, args);
+	}
+
+}
