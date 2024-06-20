cae1f7436d22aae7ec73b52daa983b28c8cf576f
sarfaraj <sarfa@192.168.1.42>
2024-06-20 18:05:48 +0530

    Eureka Server

---
+++ TrustBank-EurekaServer/src/main/java/com/yo/Trust/TrustBankEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.yo.Trust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class TrustBankEurekaServerApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TrustBankEurekaServerApplication.class, args);
+	}
+
+}
